7c859910e6d0877e6c0547fea6516ae14abef97e
michiel thai <michiel.thai@inmotiv.be>
2019-11-20 13:05:37 +0100

    👷‍♂️ Publish script to PSGAllery

---
+++ Publish-ToPSGallery.ps1
@@ -0,0 +1,28 @@
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+
+$releaseNotes = $env:RELEASE_NOTES
+$moduleVersion = $env:Gitversion_SemVerId
+Write-Host "ModuleVersion: $moduleVersion"
+
+$manifestPath = Resolve-Path -Path "*\*.psd1"
+Write-Host "Manifest Path: $manifestPath"
+
+
+Update-ModuleManifest -ReleaseNotes $releaseNotes -Path $manifestPath.Path -ModuleVersion $moduleVersion #-Verbose
+
+$moduleFilePath = Resolve-Path -Path "*\*.psm1"
+Write-Host "Module File Path: $moduleFilePath"
+
+$modulePath = Split-Path -Parent $moduleFilePath
+Write-Host "Module Path: $modulePath"
+
+$module = $modulePath | Split-Path -leaf
+
+$nuGetApiKey = $env:PSGallery_PAT
+
+try {
+    Publish-Module -Path $modulePath -NuGetApiKey $nuGetApiKey -ErrorAction Stop -Force
+    Write-Host "Module: $module Version: $moduleVersion has been Published to the PowerShell Gallery!"
+} catch {
+    throw $_
+}
