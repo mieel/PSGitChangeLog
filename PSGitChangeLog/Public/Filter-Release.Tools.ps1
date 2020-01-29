73e6538276c1189113fd34ef18ef5472e7ea501c
michiel thai <michiel.thai@inmotiv.be>
2020-01-29 15:49:39 +0100

    🎨 Create Get-FilteredRelease

---
+++ PSGitChangeLog/Public/Filter-Release.Tools.ps1
@@ -0,0 +1,34 @@
+Function Get-FilteredRelease {
+    Param (
+        $Major
+        ,
+        $Minor
+        ,
+        $Latest
+        ,
+        [Parameter(Mandatory)]
+        $Releases
+    )
+    $CurrentMajor = $Releases.Major | Select-Object -first 1
+    $CurrentMinor = ($Releases | Where-Object { $_.Major -eq $Major } ).Minor | Select-Object -first 1
+    If ($Latest) {
+        Write-Information "Getting Releases of Latest $Latest, current Major:$CurrentMajor, Minor:$CurrentMinor"
+        If ($Latest -eq 'Build') {
+            $Releases = $Releases | Select-Object -first 1
+        } ElseIf ($Latest -eq 'Major') {
+
+            Write-Information "$latest $major"
+            $Releases = $Releases | Where-Object { $_.Major -eq $CurrentMajor }
+        } ElseIf ($Latest -eq 'Minor') {
+            Write-Information "$latest $Major.$Minor"
+            $Releases = $Releases | Where-Object { $_.Major -eq $CurrentMajor -and $_.Minor -eq $CurrentMinor }
+        }
+    }
+    If ($Major) {
+        $Releases = $Releases | Where-Object { $_.Major -eq $Major }
+    }
+    If ($Minor) {
+        $Releases = $Releases | Where-Object { $_.Major -eq $Major -and $_.Minor -eq $Minor }
+    }
+    Write-Output $Releases
+}
